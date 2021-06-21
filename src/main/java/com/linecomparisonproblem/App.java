a3477174c49dd8dce4ec56e13ce92c0926f59e2c
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-21 20:00:49 +0530

    Added: Welcome To Line Comparison Computation

---
+++ src/main/java/com/linecomparisonproblem/App.java
@@ -0,0 +1,9 @@
+package com.linecomparisonproblem;
+
+class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Welcome to Line Comparison Computation" );
+    }
+}
