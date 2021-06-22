a62d747e0e9e591aa856f7a3cf9d939a740fc15f
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-22 07:56:55 +0530

    Updated: Comparing Two Lines

--- src/main/java/com/linecomparisonproblem/App.java
+++ src/main/java/com/linecomparisonproblem/App.java
@@ -18,18 +18,32 @@ class App
         int line2y1=0;
         
         int line2x2=0;
-        int line2y2=8;
+        int line2y2=10;
         
         Double LengthOfLine2  = Math.sqrt(Math.pow((line2x2-line2x1),2) + Math.pow((line2y2-line2y1),2));
         
         System.out.println("The lenght of line is :"+ LengthOfLine2 );
         
+//        Checking 2 Lines are Equal
         if ( LengthOfLine1.equals(LengthOfLine2)  ) {
         	System.out.println("The Two Line are equal");
         }
         else
         {
         	System.out.println("The Two Lines Are Not Equal");
+        }
+
+//        Comparing 2 lines
+        int compare = LengthOfLine1.compareTo(LengthOfLine2);
+        if ( compare == 0) {
+        	System.out.println("The Two Line are equal");
+        }
+        else if(compare < 0){
+        	System.out.println("Line 2 is greater than Line 1 ");
+        }
+        else
+        {
+        	System.out.println("Line 1 is greater than Line 2");
         }
     }
 }
